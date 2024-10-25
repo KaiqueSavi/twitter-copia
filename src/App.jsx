c1d8558fa8366df2a1995fd9fe5e6684459c53f3
Kaique Savi <kaiquesavi1@gmail.com>
2024-10-24 22:41:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Sidebar from '/src/components/Sidebar'
+
+function App() {
+
+  return (
+    
+    <div className='flex mx-auto max-w-7xl'>
+      <Sidebar />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+export function Sidebar(){
+
+    return(
+        <div className="w-20 xl:w-64 sticky top-0 px-2 h-screen">
+            sidebar
+        </div>
+    )
+
+}
+
+export default Sidebar
